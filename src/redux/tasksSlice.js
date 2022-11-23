4d95edb9daea25e2d0d435acdf4857c9230fdccf
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-23 11:24:22 +0300

    fix

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -3,6 +3,20 @@ import { persistReducer } from 'redux-persist';
 import { fetchContact, deleteContactApi, addContactApi } from './operations/operations';
 import storage from 'redux-persist/lib/storage';
 
+const handlePending = state => {
+  console.log("pending");
+  state.isLoading = true;
+  state.status = 'loading';
+  state.error = null;
+};
+
+const handleRejected = (state, action) => {
+  console.log("rejected");
+  state.isLoading = true;
+  state.status = 'rejected';
+  state.error = action.payload;
+};
+
 
 export const contactsSlice = createSlice({
   name: 'contacts',
@@ -26,40 +40,24 @@ export const contactsSlice = createSlice({
     },
   },
   extraReducers: {
-    [fetchContact.pending]: state => {
-      console.log("fetchContact.pending");
-      state.status = 'loading';
-      state.error = null;
-    },
+    [fetchContact.pending]: handlePending,
+    [deleteContactApi.pending]: handlePending,
+    [addContactApi.pending]: handlePending,
+    [fetchContact.rejected]: handleRejected,
+    [deleteContactApi.rejected]: handleRejected,
+    [addContactApi.rejected]: handleRejected,
     [fetchContact.fulfilled]: (state, action) => {
     console.log("fetchContact.fulfilled");
       state.status = 'resolved';
+      state.isLoading = false;
       state.error = null;
       state.items = action.payload.data;
-    },
-    [fetchContact.rejected]: (state, action) => {
-      console.log("fetchContact.rejected");
-      state.status = 'rejected';
-      state.error = action.payload;
-    },
-    [addContactApi.pending](state) {
-      console.log("addContactApi.pending");
-      state.isLoading = true;
     },
     [addContactApi.fulfilled](state, action) {
       console.log("addContactApi.fulfilled");
       state.isLoading = false;
       state.error = null;
       state.items.push(action.payload);
-    },
-    [addContactApi.rejected](state, action) {
-      console.log("addContactApi.rejected");
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    [deleteContactApi.pending](state) {
-      console.log("deleteContactApi.pending");
-      state.isLoading = true;
     },
     [deleteContactApi.fulfilled](state, action) {
       console.log("deleteContactApi.fulfilled");
@@ -69,11 +67,6 @@ export const contactsSlice = createSlice({
         task => task.id === action.payload.id
       );
       state.items.splice(index, 1);
-    },
-    [deleteContactApi.rejected](state, action) {
-      console.log("deleteContactApi.rejected");
-      state.isLoading = false;
-      state.error = action.payload;
     },
   },
 });
